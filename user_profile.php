042f84a3da6b702eda18a90edd5514514158b3ee
Matas <92822344+MatasLuc@users.noreply.github.com>
2026-02-28 19:12:54 +0200

    Update user_profile.php

--- user_profile.php
+++ user_profile.php
@@ -2,7 +2,11 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-session_start();
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 require __DIR__ . '/db.php';
 require __DIR__ . '/layout.php';
 
