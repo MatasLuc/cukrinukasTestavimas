82612c96e7c648e4c6010bfac27f4f1e10bd9964
Matas <92822344+MatasLuc@users.noreply.github.com>
2026-03-26 11:16:24 +0000

    Refactor feature descriptions and update social login buttons in login and register pages

--- register.php
+++ register.php
@@ -305,15 +305,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <ul class="feature-list">
                 <li class="feature-item">
                     <div class="feature-icon">✨</div>
-                    <span>Nemokama narystė</span>
+                    <span>Narystė nemokama</span>
                 </li>
                 <li class="feature-item">
                     <div class="feature-icon">🚚</div>
-                    <span>Greitesnis atsiskaitymas</span>
+                    <span>Draugiška bendruomenė</span>
                 </li>
                 <li class="feature-item">
                     <div class="feature-icon">🎁</div>
-                    <span>Kaupiamoji nuolaidų sistema</span>
+                    <span>Išskirtiniai pasiūlymai</span>
                 </li>
             </ul>
         </div>
@@ -326,11 +326,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             <div class="social-buttons">
                 <div id="google-btn-container"></div>
-                
-                <button type="button" class="btn-facebook" onclick="loginWithFacebook()">
-                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/></svg>
-                    Registruotis su Facebook
-                </button>
             </div>
 
             <div class="auth-divider">arba su el. paštu</div>
